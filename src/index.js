6e4460abba93aaba0dc36c897d815e4950cf3edc
Daniil Yuzhanin <daniilyuzhanin@gmail.com>
2020-10-03 22:25:01 +0300

    feat: add avg

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
 
 exports.min = function min (array) {
 	if (array == undefined || array.length == 0) {
-	return 0;
-   } else {
-		let minNum = array[0];
-		for (let i = 0; i < array.length; i++) {
-			if (array[i] < minNum) {
-				minNum = array[i];
+		return 0;
+		} else {
+			let minNum = array[0];
+			for (let i = 0; i < array.length; i++) {
+				if (array[i] < minNum) {
+					minNum = array[i];
+				}
 			}
-	 	}
-	}
-	return minNum;
+			return minNum;
+		}
 };
 
 exports.max = function max (array) {
 	if (array == undefined || array.length == 0) {
-	return 0;
-	} else {
-		let maxNum = array[0];
-		for (let i = 0; i < array.length; i++) {
-			if (array[i] > maxNum) {
-				maxNum = array[i];
+		return 0;
+		} else {
+			let maxNum = array[0];
+			for (let i = 0; i < array.length; i++) {
+				if (array[i] > maxNum) {
+					maxNum = array[i];
+				}
 			}
+			return maxNum;
 		}
-	}
-	return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	let sum = 0;
+	if (array == undefined || array.length == 0) {
+		return 0;
+		} else {
+			let avgNum = array[0];
+			for (let i = 0; i < array.length; i++) {
+				sum += array[i];
+				avgNum = sum / array.length;
+			}
+			return avgNum;
+		}
 }
